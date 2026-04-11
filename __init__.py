8f4d70112249a464f8362134d64868bce151e16e
lapulane <3071678226@qq.com>
2026-04-11 15:06:59 +0800

    回退

--- __init__.py
+++ __init__.py
@@ -1,11 +1,2 @@
-from astrbot.api.plugin import AstrBotPlugin as Plugin  # 新版，正确
-
-class RandomJokes(Plugin):
-    def __init__(self):
-        super().__init__()
-        self.metadata = ...
-    
-    async def on_load(self):
-        ...
-
-# 这里绝对不能写 __plugin__ = RandomJokes()
+from .main import RandomJokes
+__plugin__ = RandomJokes
